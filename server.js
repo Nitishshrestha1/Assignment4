e7fd7f17bb271702a749f2e45f5d43f0819f4913
Nitishshrestha1 <nitishshrestha159@gmail.com>
2025-12-03 22:15:48 -0500

    working on it

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
@@ -9,7 +10,7 @@ app.use(express.static("public")); // allow images to be served
 
 const storage = multer.diskStorage({
     distination: (req,file, cb) => {
-        return cb(null, "./image");
+        return cb(null, "./public");
     },
     filename: function (req, file, cb) {
         return cb(null, `${file.originalname}`);
@@ -20,7 +21,7 @@ const upload = multer({storage })
 
 app.use(express.json())
 // API route to return the correct image
-app.get("/api/getImage", upload.single('pic'), (req, res) => {
+app.get("/api/getImage", (req, res) => {
     const name = req.query.name.toLowerCase();  //name = jerry
 
     let image = "default.jpg";
@@ -32,8 +33,16 @@ app.get("/api/getImage", upload.single('pic'), (req, res) => {
     res.json({ url: "/" + image });   
 });
 
-app.post('/api/upload',(req,res) => {
+app.post('/api/upload', upload.single('image'),(req,res) => {
+    const name = req.query.name;
 
+    if (!name) return res.status(400).send('Missing ?name=');
+    if (!req.file) return res.status(400).send('No file uploaded');
+
+    const filename = `${name}.jpg`;
+    const filepath = path.join(__dirname, 'public', filename);
+
+    res.send(req.body);
 })
 
 // Start server
